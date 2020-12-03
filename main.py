386d759d083beb88d48a9c69f2ca11e348c115db
bencw12 <59675113+bencw12@users.noreply.github.com>
2020-12-03 15:46:24 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 from NetworkCUDA import Network
+#from Network import Network
 from NumberGuesser import NumberGuesser
 from mnist import MNIST
-import cupy as cp
+import numpy as np
 
 
 mndata = MNIST('samples')
 images, labels = mndata.load_training()
 test_images, test_labels = mndata.load_testing()
-test_images = (cp.array(test_images)/127.5) - 1.0
-test_labels = cp.array(test_labels)
-train_images = (cp.array(images))
+test_images = (np.array(test_images)/127.5) - 1.0
+test_labels = np.array(test_labels)
+train_images = (np.array(images))
 
 train_label_vecs = []
 
@@ -20,15 +21,18 @@ for x in labels:
 	vec[x] = 1
 	train_label_vecs.append(vec)
 
-train_label_vecs = cp.array(train_label_vecs)
+train_label_vecs = np.array(train_label_vecs)
 # INITIALIZE THE NETWORK 
-net = Network([784, 1000, 500, 10], train_images, train_label_vecs, test_images, test_labels)
+#net = Network([784, 1000, 500, 10], train_images, train_label_vecs, test_images, test_labels)
 
 # TRAINING (Achieves 99% Accuracy)
-net.SGD(epochs = 30, mini_batch_size = 10, lr = 0.5, lmbda = 0.5)
-net.save('network-new')
+#net.SGD(epochs = 30, mini_batch_size = 10, lr = 0.5, lmbda = 0.5)
+#net.save('network-new')
 
-trained_net = Network.load('network-final', train_images, train_label_vecs,	 test_images, test_labels)
+# FOR CUDA
+trained_net = Network.load('network-CUDA', train_images, train_label_vecs,	 test_images, test_labels)
+#trained_net = Network.load('network', train_images, train_label_vecs,	 test_images, test_labels)
+
 
 # EVALUATE
 #print(net.evaluate())
