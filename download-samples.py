fa394d3089cca3b3596f8b680c866c401258727f
bencw12 <59675113+bencw12@users.noreply.github.com>
2021-01-18 12:06:15 -0500

    Update download-samples.py

--- download-samples.py
+++ download-samples.py
@@ -3,6 +3,9 @@ import urllib.request
 import gzip
 import shutil
 
+subprocess.check_call([sys.executable, '-m', 'pip', 'install', 
+'python-mnist', 'keras', 'tensorflow==2.2', 'pygame', 'scikit-image', 'matplotlib'])
+
 try:
 	os.mkdir('samples')
 except:
@@ -16,7 +19,7 @@ url4 = 'http://yann.lecun.com/exdb/mnist/t10k-labels-idx1-ubyte.gz'
 urllib.request.urlretrieve(url1, 'train-images-idx3-ubyte.gz')
 urllib.request.urlretrieve(url2, 'train-labels-idx1-ubyte.gz')
 urllib.request.urlretrieve(url3, 't10k-images-idx3-ubyte.gz')
-urllib.request.urlretrieve(url4, 't10k-labels-idx1-ubyte.gz')\
+urllib.request.urlretrieve(url4, 't10k-labels-idx1-ubyte.gz')
 
 with gzip.open('train-images-idx3-ubyte.gz', 'rb') as f_in:
     with open('train-images-idx3-ubyte', 'wb') as f_out:
@@ -24,7 +27,7 @@ with gzip.open('train-images-idx3-ubyte.gz', 'rb') as f_in:
 
 with gzip.open('train-labels-idx1-ubyte.gz', 'rb') as f_in:
     with open('train-labels-idx1-ubyte', 'wb') as f_out:
-        shutil.copyfileobj(f_in, f_out)\
+        shutil.copyfileobj(f_in, f_out)
 
 with gzip.open('t10k-images-idx3-ubyte.gz', 'rb') as f_in:
     with open('t10k-images-idx3-ubyte', 'wb') as f_out:
@@ -34,3 +37,7 @@ with gzip.open('t10k-labels-idx1-ubyte.gz', 'rb') as f_in:
     with open('t10k-labels-idx1-ubyte', 'wb') as f_out:
         shutil.copyfileobj(f_in, f_out)
 
+os.remove('train-images-idx3-ubyte.gz')
+os.remove('train-labels-idx1-ubyte.gz')
+os.remove('t10k-images-idx3-ubyte.gz')
+os.remove('t10k-labels-idx1-ubyte.gz')
